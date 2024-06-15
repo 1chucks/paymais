0cbda7e0df1fa4ca77ff50b7562c4e1c5aad7d69
Philix27 <philixbob@gmail.com>
2024-06-15 19:54:06 +0100

    feat[server]: added documents, wallet and notification modules

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,13 +1,17 @@
-import { Hono } from "hono";
-import { BlankEnv, BlankSchema } from "hono/types";
-import { indexRoutes } from "./home";
-import { authRoutes } from "../mod";
+import { Hono } from "hono"
+import { BlankEnv, BlankSchema } from "hono/types"
+
+import { authRoutes, clusterRoutes, userRoutes, walletRoutes } from "../modules"
+import { indexRoutes } from "./home"
 
 export function registerRoutes(app: Hono<BlankEnv, BlankSchema, "/">) {
   const apiRoutes = app
     .basePath("/api")
     .route("/", indexRoutes)
-    .route("/auth", authRoutes);
+    .route("/auth", authRoutes)
+    .route("/cluster", clusterRoutes)
+    .route("/user", userRoutes)
+    .route("/wallets", walletRoutes)
 
-  return apiRoutes;
+  return apiRoutes
 }
